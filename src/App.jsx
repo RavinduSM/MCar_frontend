b1ff4dd51af098b9011e4371d66720da1f874d37
RavinduSM <ravindumuthukudaarachchi@gmail.com>
2024-05-03 12:48:40 +0530

    Revert "slider removed"
    This reverts commit e033458e1d14744e9f16cf95a31b2de6e09106fa.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import "./index.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Homes/Home";
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import Navbar from "./components/Navbar";
 import Vehicle from "./Pages/Vehicle/Vehicle";
 >>>>>>> parent of 106a048 (Revert "RTK Query setup")
+=======
+>>>>>>> parent of e033458 (slider removed)
 
 const App = () => {
   return (
