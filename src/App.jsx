e6d6dd47dcd7df2cd74a43e05cb02118ac625bdd
RavinduSM <ravindumuthukudaarachchi@gmail.com>
2024-05-03 12:59:48 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-import React from "react";
 import "./index.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Homes/Home";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import Navbar from "./components/Navbar";
-import Vehicle from "./Pages/Vehicle/Vehicle";
->>>>>>> parent of 106a048 (Revert "RTK Query setup")
-=======
->>>>>>> parent of e033458 (slider removed)
 
 const App = () => {
   return (
     <div className="">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/vehicles" element={<Vehicle />} />
       </Routes>
     </div>
   );
 };
-
 export default App;
